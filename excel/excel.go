2fbb76d236aafca6426d4825c37eaf3059b02264
Patrick Tran <pho.tran@productaddon.com>
2025-08-18 12:43:50 +0700

    Add ConvertFlatJSONToExcel function

---
+++ excel/excel.go
@@ -0,0 +1,101 @@
+package excel
+
+import (
+	"errors"
+	"fmt"
+	helper "github.com/patricktran149/Helper"
+	"github.com/xuri/excelize/v2"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// ConvertFlatJSONToExcel converts either a JSON flat object or array of flat objects into Excel
+// and forces all cells to be stored as text
+func ConvertFlatJSONToExcel(jsonStr, filePath string) error {
+	// Try to unmarshal into an array first
+	var arr = make([]bson.D, 0)
+	if err := bson.UnmarshalExtJSON([]byte(jsonStr), true, &arr); err == nil {
+		if len(arr) > 0 {
+			return writeToExcel(arr, filePath)
+		}
+	}
+
+	//Try single object
+	var obj = make(bson.D, 0)
+	if err := bson.UnmarshalExtJSON([]byte(jsonStr), true, &obj); err == nil {
+		return writeToExcel([]bson.D{obj}, filePath)
+	} else {
+		return errors.New("Parse JSON ERROR - " + err.Error())
+	}
+}
+
+// Helper: write array of maps into Excel
+func writeToExcel(data []bson.D, filePath string) error {
+	// Collect all keys
+	seen := make(map[string]struct{})
+	var keys []string
+	for _, doc := range data {
+		for _, elem := range doc {
+			if _, ok := seen[elem.Key]; !ok {
+				seen[elem.Key] = struct{}{}
+				keys = append(keys, elem.Key)
+			}
+		}
+	}
+
+	// Create Excel file
+	f := excelize.NewFile()
+	sheet := f.GetSheetName(0)
+
+	// Define text style (@ means text format in Excel)
+	textStyle, err := f.NewStyle(&excelize.Style{NumFmt: 49})
+	if err != nil {
+		return err
+	}
+
+	// Header row
+	for col, key := range keys {
+		colLetter, _ := excelize.ColumnNumberToName(col + 1)
+		cell := fmt.Sprintf("%s1", colLetter)
+		f.SetCellValue(sheet, cell, key)
+		f.SetCellStyle(sheet, cell, cell, textStyle)
+	}
+
+	// Data rows
+	for row, obj := range data {
+		for col, key := range keys {
+			var val interface{}
+
+			for _, field := range obj {
+				if field.Key == key {
+					val = field.Value
+				}
+			}
+
+			colLetter, _ := excelize.ColumnNumberToName(col + 1)
+			cell := fmt.Sprintf("%s%d", colLetter, row+2)
+			f.SetCellStyle(sheet, cell, cell, textStyle)
+
+			if val == nil {
+				continue
+			}
+
+			var strVal string
+
+			switch v := val.(type) {
+			case string:
+				strVal = v // keep as is, no quotes
+			default:
+				strVal = helper.JSONToString(v) // numbers, bool, etc.
+			}
+
+			f.SetCellStr(sheet, cell, fmt.Sprintf("%v", strVal)) // force as string
+		}
+	}
+
+	// Save file
+	if err = f.SaveAs(filePath); err != nil {
+		return errors.New("Excel Save As ERROR - " + err.Error())
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/patricktran149/Helper
 
-go 1.22.1
+go 1.23.0
 
-toolchain go1.22.2
+toolchain go1.24.6
 
 require (
 	github.com/SAP/go-hdb v1.8.11
@@ -22,8 +22,8 @@ require (
 	github.com/sijms/go-ora/v2 v2.7.24
 	go.elastic.co/apm/module/apmsql v1.15.0
 	go.mongodb.org/mongo-driver v1.14.0
-	golang.org/x/crypto v0.21.0
-	golang.org/x/text v0.14.0
+	golang.org/x/crypto v0.38.0
+	golang.org/x/text v0.25.0
 )
 
 require (
@@ -49,19 +49,25 @@ require (
 	github.com/prometheus/procfs v0.13.0 // indirect
 	github.com/qntfy/jsonparser v1.0.2 // indirect
 	github.com/qntfy/kazaam v3.4.9+incompatible // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
+	github.com/tiendc/go-deepcopy v1.6.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/xuri/efp v0.0.1 // indirect
+	github.com/xuri/excelize/v2 v2.9.1 // indirect
+	github.com/xuri/nfp v0.0.1 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.elastic.co/apm v1.15.0 // indirect
 	go.elastic.co/fastjson v1.1.0 // indirect
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 )
